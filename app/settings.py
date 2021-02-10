d5b332966277e7f368970e803cc231a534856896
florm2496 <florm2496@gmail.com>
2021-02-10 15:52:49 -0300

    estaticos

--- app/settings.py
+++ app/settings.py
@@ -115,7 +115,7 @@ TIME_ZONE='America/Argentina/Buenos_Aires'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT=os.path.join(BASE_DIR ,'/staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR ,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR ,'static'),)
 
